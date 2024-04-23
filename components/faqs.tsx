040159da26f9a4a35baaf8b3a48dd8f6f146978d
Eli4545 <elisimon4545@gmail.com>
2024-04-23 07:12:01 +0100

    worked on collaboration and partnership section

--- components/faqs.tsx
+++ components/faqs.tsx
@@ -13,36 +13,40 @@ export default function Faqs() {
             <div className="w-full md:w-1/2 space-y-8">
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-cabinet-grotesk font-bold">What is the difference between the Free and Paid versions?</h4>
-                <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.</p>
+                <h4 className="text-xl font-cabinet-grotesk font-bold">What is the Prince Ponfa Miri Foundation about?</h4>
+                <p className="text-gray-500">Is Non-Governmental Organization (NGO).It is a Youth-Based Civic Innovation Lab & Policy Research Think-Tank That works with organizations, elected representatives &policymakers to ensure the effective implementation of the United Nations 2030 Agenda for Sustainable Development along with Africa 2030</p>
               </div>
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-cabinet-grotesk font-bold">Do you have student or non-profit discounts?</h4>
-                <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.</p>
+                <h4 className="text-xl font-cabinet-grotesk font-bold">How does the Prince Ponfa Miri Foundation aim to bridge the gap between top-down governance and grassroots engagement?</h4>
+                <p className="text-gray-500">The foundation employs a holistic approach by involving various stakeholders, including policymakers, private investors, technologists, and grassroots communities, to ensure that development initiatives are tailored to local needs and aspirations.</p>
               </div>
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-cabinet-grotesk font-bold">How is the price determined?</h4>
-                <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.</p>
+                <h4 className="text-xl font-cabinet-grotesk font-bold">What role does youth participation play in the foundation's mission?</h4>
+                <p className="text-gray-500">The foundation believes in bringing today's youth to the forefront of development by involving them in policy-making and governance processes. Youth participation is crucial for ensuring the sustainability and relevance of development initiatives at the grassroots leve.</p>
               </div>
             </div>
             {/* Column */}
             <div className="w-full md:w-1/2 space-y-8">
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-cabinet-grotesk font-bold">Do I need coding knowledge to use this product?</h4>
-                <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.</p>
+                <h4 className="text-xl font-cabinet-grotesk font-bold">How does PPMF identify priorities for sustainable development interventions?</h4>
+                <p className="text-gray-500">PPMF conducts thorough research and engagement with grassroots communities to identify actual needs, issues, and aspirations. This bottom-up approach ensures that interventions are targeted and responsive to the specific context of each constituency.</p>
               </div>
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-cabinet-grotesk font-bold">Is there a way to become an Affiliate reseller?</h4>
-                <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.</p>
+                <h4 className="text-xl font-cabinet-grotesk font-bold">What is the significance of Constituency Development through UNSDGs in PPMF's work?</h4>
+                <p className="text-gray-500">Constituency Development through UNSDGs is central to PPMF's approach as it enables the outreach of social impact schemes to the grassroots level. By aligning development efforts with the UN Sustainable Development Goals, PPMF ensures a comprehensive and globally recognized framework for sustainable development.</p>
               </div>
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-cabinet-grotesk font-bold">What forms of payment do you accept?</h4>
-                <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.</p>
+                <h4 className="text-xl font-cabinet-grotesk font-bold">How does PPMF collaborate with diverse stakeholders to achieve its goals?</h4>
+                <p className="text-gray-500">PPMF collaborates with a wide range of stakeholders, including organizations, elected representatives, policymakers, administrators, private investors, technologists, and change makers. This collaborative approach fosters synergy and leverages expertise and resources for effective implementation of development initiatives.</p>
+              </div>
+              <div className="space-y-2">
+                <h4 className="text-xl font-cabinet-grotesk font-bold"> What is PPMF's vision for addressing the United Nations' 2030 Agenda for Sustainable Development and Africa 2030?</h4>
+                <p className="text-gray-500">PPMF's vision is to adopt a holistic strategy that integrates top-down governance with grassroots engagement to achieve the objectives of the UN's 2030 Agenda and Africa's development aspirations. By providing essential support in policy-making and analytical research, PPMF aims to facilitate the implementation of UNSDGs at the grassroots level.</p>
               </div>
             </div>
           </div>
